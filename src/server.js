6a1520df2459b7de85719c8fc0c61751010f6a85
Abdulsami12166 <140266947+Abdulsami12166@users.noreply.github.com>
2026-06-02 10:40:57 +0530

    Use separate admin backend port

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const connectDB = require('./config/db');
 const { logger } = require('./utils/logger');
 const { attachSocketServer } = require('./utils/eventBus');
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5002;
 
 const startServer = async () => {
   try {
